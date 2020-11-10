7f7ee6ffcd2abad562520a9288887a43dcec2b40
Anthony <ayip2151@uni.sydney.edu.au>
2020-11-10 22:03:18 +1100

    Web template

---
+++ js/script.js
@@ -0,0 +1,66 @@
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+// scroll down arrow animation
+gsap.to(".arrow", { y: 12, ease: "power1.inOut", repeat: -1, yoyo: true });
+
+// --- PROGRESS BAR ---
+gsap.to('progress', {
+    value: 100,
+    ease: 'none',
+    scrollTrigger: { scrub: 0.3 }
+});
+
+
+
+// --- RED PANEL ---
+gsap.from(".line-1", {
+    scrollTrigger: {
+        trigger: ".line-1",
+        scrub: true,
+        start: "top bottom",
+        end: "top top"
+    },
+    scaleX: 0,
+    transformOrigin: "left center",
+    ease: "none"
+});
+
+
+// --- ORANGE PANEL ---
+gsap.from(".line-2", {
+    scrollTrigger: {
+        trigger: ".orange",
+        scrub: true,
+        pin: true,
+        start: "top top",
+        end: "+=100%"
+    },
+    scaleX: 0,
+    transformOrigin: "left center",
+    ease: "none"
+});
+
+
+// --- PURPLE/GREEN PANEL ---
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".purple",
+        scrub: true,
+        pin: true,
+        start: "top top",
+        end: "+=100%"
+    }
+});
+
+tl.from(".purple p", { scale: 0.3, rotation: 45, autoAlpha: 0, ease: "power2" })
+    .from(".line-3", { scaleX: 0, transformOrigin: "left center", ease: "none" }, 0)
+    .to(".purple", { backgroundColor: "#28a92b" }, 0);
+
+
+
+gsap.utils.toArray("nav a").forEach(function(a) {
+    a.addEventListener("click", function(e) {
+        e.preventDefault();
+        gsap.to(window, { duration: 1, scrollTo: e.target.getAttribute("href") });
+    });
+});
