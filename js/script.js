166f3500e6b21cadb65b5c00a87fc290ae0ed7c0
Anthony <ayip2151@uni.sydney.edu.au>
2020-11-18 01:30:45 +1100

    Lofi-remake; still not reponsive

--- js/script.js
+++ js/script.js
@@ -1,72 +1,39 @@
+// // Resize window, responsive
+// function resize() {
+//   let height = window.innerHeight;
+//   var panels = document.querySelectorAll(".panel");
+//   var i;
+//   for (i = 0; i < panels.length; i++) {
+//     panels[i].style.setProperty("height", `${height}px`);
+//   }
+// }
+
+// // Resize on load
+// resize();
+
+// // We listen to the resize event
+// window.addEventListener('resize', resize());
+
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 ScrollTrigger.defaults({
-    toggleActions: "restart pause resume pause"
+  toggleActions: "restart pause resume pause"
 });
 
-// scroll down arrow animation
+// Scroll down arrow animation
 gsap.to(".arrow", { y: 12, ease: "power1.inOut", repeat: -1, yoyo: true });
 
 // --- PROGRESS BAR ---
-gsap.to('progress', {
-    value: 100,
-    ease: 'none',
-    scrollTrigger: { scrub: 0.3 }
+gsap.to("progress", {
+  value: 100,
+  ease: "none",
+  scrollTrigger: { scrub: 0.3 }
 });
 
-
-
-// --- RED PANEL ---
-gsap.from(".line-1", {
-    scrollTrigger: {
-        trigger: ".line-1",
-        scrub: true,
-        start: "top bottom",
-        end: "top top"
-    },
-    scaleX: 0,
-    transformOrigin: "left center",
-    ease: "none"
-});
-
-
-// --- ORANGE PANEL ---
-gsap.from(".line-2", {
-    scrollTrigger: {
-        trigger: ".orange",
-        scrub: true,
-        pin: true,
-        start: "top top",
-        end: "+=100%",
-        pinSpacing: false
-    },
-    scaleX: 0,
-    transformOrigin: "left center",
-    ease: "none"
-});
-
-
-// --- PURPLE/GREEN PANEL ---
-var tl = gsap.timeline({
-    scrollTrigger: {
-        trigger: ".purple",
-        scrub: true,
-        pin: true,
-        start: "top top",
-        end: "+=100%",
-        pinSpacing: false
-    }
-});
-
-tl.from(".purple p", { scale: 0.3, rotation: 45, autoAlpha: 0, ease: "power2" })
-    .from(".line-3", { scaleX: 0, transformOrigin: "left center", ease: "none" }, 0)
-    .to(".purple", { backgroundColor: "#28a92b" }, 0);
-
-
-
-gsap.utils.toArray("nav a").forEach(function(a) {
-    a.addEventListener("click", function(e) {
-        e.preventDefault();
-        gsap.to(window, { duration: 0.5, scrollTo: e.target.getAttribute("href") });
-    });
+// Navigation bar
+gsap.utils.toArray("nav a").forEach(function (a) {
+  a.addEventListener("click", function (e) {
+    e.preventDefault();
+    gsap.to(window, { duration: 0.5, scrollTo: e.target.getAttribute("href") });
+  });
 });
