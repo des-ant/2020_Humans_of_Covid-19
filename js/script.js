00c1d77d0e5d06cdde730ffc769154a76cc857b2
Anthony <ayip2151@uni.sydney.edu.au>
2020-11-26 18:52:43 +1100

    responsive svg

--- js/script.js
+++ js/script.js
@@ -1,24 +1,4 @@
-// // Resize window, responsive
-// function resize() {
-//   let height = window.innerHeight;
-//   var panels = document.querySelectorAll(".panel");
-//   var i;
-//   for (i = 0; i < panels.length; i++) {
-//     panels[i].style.setProperty("height", `${height}px`);
-//   }
-// }
-
-// // Resize on load
-// resize();
-
-// // We listen to the resize event
-// window.addEventListener('resize', resize());
-
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-
-// ScrollTrigger.defaults({
-//   toggleActions: "restart pause resume pause"
-// });
 
 // Scroll down arrow animation
 gsap.to(".arrow", { y: 12, ease: "power1.inOut", repeat: -1, yoyo: true });
@@ -51,49 +31,40 @@ gsap.utils.toArray('.step').forEach(step => {
   });
 });
 
-// This pins the SVG animation wrapper when it hits the center of the viewport
-// and releases the pin when the final textbox meets the bottom of the svg
-// We use a function to define the end point to line up the top of the
-// title box with the bottom of the svg
-// ScrollTrigger.create({
-//   trigger: '#svg-1-1',
-//   endTrigger: '#title-2',
-//   start: 'center center',
-//   end: () => {
-//     const height = window.innerHeight;
-//     const svgHeight = document.querySelector('#svg-1-1')
-//       .offsetHeight;
-//     return `top ${svgHeight + (height - svgHeight) / 2}px`;
-//   },
-//   pin: true,
-//   pinSpacing: false,
-//   // markers: true,
-//   id: 'svg-pin-1-1'
-// });
-
 // First svg animation
 // This pins the SVG animation wrapper when it hits the center of the viewport
 // and releases the pin when the final textbox meets the bottom of the svg
 // We use a function to define the end point to line up the top of the
 // title box with the bottom of the svg
+ScrollTrigger.create({
+  trigger: '#svg-1-1',
+  toggleActions: "play pause resume reset",
+  endTrigger: '#title-2',
+  start: 'center center',
+  end: () => {
+    const height = window.innerHeight;
+    const svgHeight = document.querySelector('#svg-1-1')
+      .offsetHeight;
+    return `top ${svgHeight + (height - svgHeight) / 2}px`;
+  },
+  pin: true,
+  pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+  id: 'svg-pin-1-1'
+});
+
 var t1 = gsap.timeline({
-  // repeat: 1,
+  repeat: -1,
   scrollTrigger: {
     trigger: '#svg-1-1',
+    toggleActions: "play pause resume reset",
     endTrigger: '#title-2',
-    start: 'center center',
-    end: () => {
-      const height = window.innerHeight;
-      const svgHeight = document.querySelector('#svg-1-1')
-        .offsetHeight;
-      return `top ${svgHeight + (height - svgHeight) / 2}px`;
-    },
-    pin: true,
-    pinSpacing: false,
+    start: 'top 80%',
+    end: 'top 20%',
     // markers: true,
-    scrub: true,
-    id: 'svg-pin-1-1'
+    id: 'svg-1-1'
   }
 });
 t1.to("#svg-1-1 #arms", {duration: 1, rotation: 90, x: 100}, 0)
-t1.to("#svg-1-1 #skirt", {duration: 1, scale: 2, ease:"back"}, 0)
+t1.to("#svg-1-1 #skirt", {duration: 1, scale: 2, ease:"back"}, 0.5)
