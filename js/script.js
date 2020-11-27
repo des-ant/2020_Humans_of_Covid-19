75061c90c1a0efbb2bd4fd944d00ac1526bb4a58
Anthony <ayip2151@uni.sydney.edu.au>
2020-11-27 20:11:18 +1100

    sidebar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 // Scroll down arrow animation
@@ -11,12 +12,22 @@ gsap.to("progress", {
 });
 
 // Navigation bar
-gsap.utils.toArray("nav a").forEach(function (a) {
+gsap.utils.toArray("nav ol li a").forEach(function (a) {
   a.addEventListener("click", function (e) {
     e.preventDefault();
     gsap.to(window, { duration: 0.5, scrollTo: e.target.getAttribute("href") });
   });
 });
+
+/* Set the left margin of the sidebar to 0 to make it visible on screen */
+function openNav() {
+  document.getElementById("mySidebar").style.left = "0";
+}
+
+/* Set the left margin of the sidebar to -250px to hide if from screen */
+function closeNav() {
+  document.getElementById("mySidebar").style.left = "-250px";
+}
 
 // sets up the class toggle on each scrolling text box
 // so that it becomes opaque when in view and transparent when exiting
@@ -38,7 +49,7 @@ gsap.utils.toArray('.step').forEach(step => {
 ScrollTrigger.create({
   trigger: '#svg-1-1',
   toggleActions: "play pause resume reset",
-  endTrigger: '#title-2',
+  endTrigger: '#svg-end-1-1',
   start: 'center center',
   end: () => {
     const height = window.innerHeight;
@@ -59,7 +70,7 @@ var t1 = gsap.timeline({
   scrollTrigger: {
     trigger: '#svg-1-1',
     toggleActions: "play pause resume reset",
-    endTrigger: '#title-2',
+    endTrigger: '#svg-end-1-1',
     start: 'top 80%',
     end: 'top 20%',
     // markers: true,
@@ -72,7 +83,7 @@ var t1 = gsap.timeline({
 ScrollTrigger.create({
   trigger: '#svg-2-1',
   toggleActions: "play pause resume reset",
-  endTrigger: '#step-2-4',
+  endTrigger: '#svg-end-2-1',
   start: 'center center',
   end: () => {
     const height = window.innerHeight;
@@ -82,20 +93,272 @@ ScrollTrigger.create({
   },
   pin: true,
   pinSpacing: false,
-  markers: true,
+  // markers: true,
   // scrub: true,
   id: 'svg-pin-2-1'
 });
 
-// var t2 = gsap.timeline({
-//   repeat: -1,
-//   scrollTrigger: {
-//     trigger: '#svg-2-1',
-//     toggleActions: "play pause resume reset",
-//     endTrigger: '#svg-2-4',
-//     start: 'top 80%',
-//     end: 'top 20%',
-//     // markers: true,
-//     id: 'svg-2-1'
-//   }
-// });
+var t2 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-2-1',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-2-1',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-2-1'
+  }
+});
+
+var t3 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-2-4',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-2-4',
+    start: 'top 80%',
+    end: 'bottom 20%',
+    // markers: true,
+    id: 'svg-2-4'
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '#svg-2-5',
+  toggleActions: "play pause resume reset",
+  endTrigger: '#svg-end-2-5',
+  start: 'center center',
+  end: () => {
+    const height = window.innerHeight;
+    const svgHeight = document.querySelector('#svg-2-5')
+      .offsetHeight;
+    return `top ${svgHeight + (height - svgHeight) / 2}px`;
+  },
+  pin: true,
+  pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+  id: 'svg-pin-2-5'
+});
+
+var t4 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-2-5',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-2-5',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-2-5'
+  }
+});
+
+var t5 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-3-1',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-3-1',
+    start: 'top 80%',
+    end: 'bottom 20%',
+    // markers: true,
+    id: 'svg-3-1'
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '#svg-3-2',
+  toggleActions: "play pause resume reset",
+  endTrigger: '#svg-end-3-2',
+  start: 'center center',
+  end: () => {
+    const height = window.innerHeight;
+    const svgHeight = document.querySelector('#svg-3-2')
+      .offsetHeight;
+    return `top ${svgHeight + (height - svgHeight) / 2}px`;
+  },
+  pin: true,
+  pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+  id: 'svg-pin-3-2'
+});
+
+var t6 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-3-2',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-3-2',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-3-2'
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '#svg-3-5',
+  toggleActions: "play pause resume reset",
+  endTrigger: '#svg-end-3-5',
+  start: 'center center',
+  end: () => {
+    const height = window.innerHeight;
+    const svgHeight = document.querySelector('#svg-3-5')
+      .offsetHeight;
+    return `top ${svgHeight + (height - svgHeight) / 2}px`;
+  },
+  pin: true,
+  pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+  id: 'svg-pin-3-5'
+});
+
+var t7 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-3-5',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-3-5',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-3-5'
+  }
+});
+
+var t8 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-3-7',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-3-7',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-3-7'
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '#svg-4-3',
+  toggleActions: "play pause resume reset",
+  endTrigger: '#svg-end-4-3',
+  start: 'center center',
+  end: () => {
+    const height = window.innerHeight;
+    const svgHeight = document.querySelector('#svg-4-3')
+      .offsetHeight;
+    return `top ${svgHeight + (height - svgHeight) / 2}px`;
+  },
+  pin: true,
+  pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+  id: 'svg-pin-4-3'
+});
+
+var t9 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-4-3',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-4-3',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-4-3'
+  }
+});
+
+var t10 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-4-5',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-4-5',
+    start: 'top 80%',
+    end: 'bottom 20%',
+    // markers: true,
+    id: 'svg-4-5'
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '#svg-5-1',
+  toggleActions: "play pause resume reset",
+  endTrigger: '#svg-end-5-1',
+  start: 'center center',
+  end: () => {
+    const height = window.innerHeight;
+    const svgHeight = document.querySelector('#svg-5-1')
+      .offsetHeight;
+    return `top ${svgHeight + (height - svgHeight) / 2}px`;
+  },
+  pin: true,
+  pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+  id: 'svg-pin-5-1'
+});
+
+var t11 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-5-1',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-5-1',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-5-1'
+  }
+});
+
+ScrollTrigger.create({
+  trigger: '#svg-6-1',
+  toggleActions: "play pause resume reset",
+  endTrigger: '#svg-end-6-1',
+  start: 'center center',
+  end: () => {
+    const height = window.innerHeight;
+    const svgHeight = document.querySelector('#svg-6-1')
+      .offsetHeight;
+    return `top ${svgHeight + (height - svgHeight) / 2}px`;
+  },
+  pin: true,
+  pinSpacing: false,
+  // markers: true,
+  // scrub: true,
+  id: 'svg-pin-6-1'
+});
+
+var t12 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-6-1',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-end-6-1',
+    start: 'top 80%',
+    end: 'top 20%',
+    // markers: true,
+    id: 'svg-6-1'
+  }
+});
+
+var t13 = gsap.timeline({
+  repeat: -1,
+  scrollTrigger: {
+    trigger: '#svg-8-1',
+    toggleActions: "play pause resume reset",
+    endTrigger: '#svg-8-1',
+    start: 'top 80%',
+    end: 'bottom 20%',
+    // markers: true,
+    id: 'svg-8-1'
+  }
+});
+
