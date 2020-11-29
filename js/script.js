931f0cfef58cdece2afcf078bbb5b8bfa82fdced
Anthony <ayip2151@uni.sydney.edu.au>
2020-11-29 21:23:59 +1100

    Final links & ref

--- js/script.js
+++ js/script.js
@@ -11,19 +11,27 @@ gsap.to("progress", {
   scrollTrigger: { scrub: 0.3 }
 });
 
-// // Navigation bar
-// gsap.utils.toArray("nav ol li a").forEach(function (a) {
-//   a.addEventListener("click", function (e) {
-//     e.preventDefault();
-//     gsap.to(window, { duration: 0.5, scrollTo: e.target.getAttribute("href") });
-//   });
-// });
-
+// Scroll to top button
+//Get the button:
+mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 /* Set the left margin of the sidebar to 0 to make it visible on screen */
 function openNav() {
   document.getElementById("mySidebar").style.left = "0";
 }
-
 /* Set the left margin of the sidebar to -250px to hide if from screen */
 function closeNav() {
   document.getElementById("mySidebar").style.left = "-250px";
